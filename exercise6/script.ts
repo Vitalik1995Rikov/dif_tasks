90c5d6fbb0c0927f590ae95b54e5184097aa3532
Vitalik1995Rikov <68601312+Vitalik1995Rikov@users.noreply.github.com>
2021-04-18 21:35:13 +0300

    проба с одним методом

---
+++ exercise6/script.ts
@@ -0,0 +1,32 @@
+export interface INode {
+  value: number | string;
+  next: LinkedListNode | null;
+}
+
+class LinkedListNode implements INode {
+  constructor(
+    public value: number | string,
+    public next: LinkedListNode | null = null,
+  ) {}
+}
+
+export interface INodeList {
+  head: LinkedListNode | null;
+  tail: LinkedListNode | null;
+}
+
+export class LinkedList implements INodeList {
+  public head: LinkedListNode | null = null;
+  public tail: LinkedListNode | null = null;
+
+  public prepend(value: number | string ): LinkedList {
+    const newNode = new LinkedListNode(value, this.head);
+    this.head = newNode;
+    if (!this.tail) {
+      this.tail = newNode;
+      return this;
+    }
+  }
+}
+
+
