07c0448bf8ac1d24ea6b0b9f31963460ed265648
Vitalik1995Rikov <68601312+Vitalik1995Rikov@users.noreply.github.com>
2021-04-22 00:22:09 +0300

    задание 1 - связный список на ts

--- exercise7/my-app/src/app/app.component.ts
+++ exercise7/my-app/src/app/app.component.ts
@@ -7,57 +7,111 @@ import { Component } from '@angular/core';
 })
 
 export class App {
-
-}
-
-class ListNode<T>{
-  constructor(data: T, next?: ListNode<T>) {
-    this.data = data;
-    this.next = next;
-  }
-
-  get data(): T {
-    return this.data;
-  }
-
-  set data(value: T) {
-    this.data = value;
-  }
-
-  get next(): ListNode<T> | undefined {
-    return this.next
-  }
-
-  set next(value: ListNode<T> | undefined) {
-    this.next = value;
-  }
+  redux = [];
 }
 
 
-class LinkedList {
-  private first: ListNode<T>;
-  private last: ListNode<T>;
-  private size: number; 
 
-  get size(): number {
-    return this.size;
-  }
+export interface INode {
+  value: number | string;
+  next: LinkedListNode | null;
+}
 
-  add(data: T, position: number): void {
-    if (!position) {
-      this.addToEnd(data);
-    } else {
+class LinkedListNode implements INode {
+  constructor(
+    public value: number | string,
+    public next: LinkedListNode | null = null,
+  ) {}
+}
 
+export interface INodeList {
+  head: LinkedListNode | null;
+  tail: LinkedListNode | null;
+}
+
+export class LinkedList implements INodeList {
+  public head: LinkedListNode | null = null;
+  public tail: LinkedListNode | null = null;
+
+  public prepend(value: number | string ): LinkedList {
+    const newNode = new LinkedListNode(value, this.head);
+    this.head = newNode;
+    if (!this.tail) {
+      this.tail = newNode;
+      return this;
     }
   }
 
-  private addToEnd(data: T): void {
-    if (this.size === 0) {
-      this.first = this.last = new ListNode<T>();
+  public append(value: number | string): LinkedList {
+    const newNode = new LinkedListNode(value);
+    if (!this.head || !this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+      return this;
     }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    return this;
   }
 
+  public delete(value: number | string): LinkedListNode | null {
+    if (!this.head) {
+      return null;
+    }
+    let deletedNode = null;
+    while (this.head && this.head.value === value) {
+      deletedNode = this.head;
+      this.head = this.head.next;
+    }
+    let currentNode = this.head;
+    if (currentNode !== null) {
+      while (currentNode.next) {
+        if (currentNode.next.value === value) {
+          deletedNode = currentNode.next;
+          currentNode.next = currentNode.next.next;
+        } else {
+          currentNode = currentNode.next;
+        }
+      }
+    }
+    if (this.tail && this.tail.value === value) {
+      this.tail = currentNode;
+    }
+    return deletedNode;
+  }
+
+
+  public find(value?: number | string | {} | undefined): LinkedListNode | null {
+    if (!this.head) {
+      return null;
+    }
+    let currentNode: LinkedListNode | null = this.head;
+    while (currentNode) {
+      if (value !== undefined && currentNode.value === value) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+
+  public toArray(): LinkedListNode[] {
+    const nodes = [];
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+
+    return nodes;
+  }
 }
 
-let arr = new LinkedList();
-arr.add(4, 4);
+
+let list = new LinkedList();
+list.prepend('0');
+list.append('1');
+console.log(list.find('0'));
+console.log(list);
