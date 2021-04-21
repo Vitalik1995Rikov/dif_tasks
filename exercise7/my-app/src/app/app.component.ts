b38230ed5b2f91b4fc45299fa37ec7273c3a2cd5
Vitalik1995Rikov <68601312+Vitalik1995Rikov@users.noreply.github.com>
2021-04-21 22:34:37 +0300

    начало

--- exercise7/my-app/src/app/app.component.ts
+++ exercise7/my-app/src/app/app.component.ts
@@ -5,12 +5,59 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
-  needRed = false;
+
+export class App {
+
+}
+
+class ListNode<T>{
+  constructor(data: T, next?: ListNode<T>) {
+    this.data = data;
+    this.next = next;
+  }
+
+  get data(): T {
+    return this.data;
+  }
+
+  set data(value: T) {
+    this.data = value;
+  }
+
+  get next(): ListNode<T> | undefined {
+    return this.next
+  }
+
+  set next(value: ListNode<T> | undefined) {
+    this.next = value;
+  }
+}
 
 
-changeTitle(): void {
-  this.needRed = !this.needRed;
+class LinkedList {
+  private first: ListNode<T>;
+  private last: ListNode<T>;
+  private size: number; 
+
+  get size(): number {
+    return this.size;
+  }
+
+  add(data: T, position: number): void {
+    if (!position) {
+      this.addToEnd(data);
+    } else {
+
+    }
+  }
+
+  private addToEnd(data: T): void {
+    if (this.size === 0) {
+      this.first = this.last = new ListNode<T>();
+    }
+  }
+
 }
-}
+
+let arr = new LinkedList();
+arr.add(4, 4);
